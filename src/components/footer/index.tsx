9a79864cec4528e24d44c0daa20a8d7b9d5a3bdd
Gabi Possiderio <gabipossiderio@gmail.com>
2023-12-08 11:11:28 -0300

    link adjustments

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -23,7 +23,7 @@ export function Footer() {
           <div className="flex justify-center py-2s">
             {" "}
             <a
-              href="https://gabislinks.vercel.app/cadastro"
+              href="https://links.gabis.dev/cadastro"
               rel="noopener noreferrer"
               className="menu text-xs text-gray-900/90 hover:scale-105 hover:text-sky-700 transition-all text-center gap-1 flex max-w-xs justify-center items-center"
               target="_blank"
@@ -38,7 +38,7 @@ export function Footer() {
         <small className=" text-xs menu text-gray-700/70">
           Todos os direitos reservados &#xa9; - {currentYear} - Desenvolvido por{" "}
           <a
-            href="https://gabislinks.vercel.app/profile/8JdYM56zWSNofa5QBKKIWZRyGAt2"
+            href="https://links.gabis.dev/profile/8JdYM56zWSNofa5QBKKIWZRyGAt2"
             className="text-sky-700 hover:text-white"
           >
             GabisDev
